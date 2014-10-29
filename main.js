42dbadb5ae9c5b1767207c9a5b4cd377109735cb
Denis Nazarov <zvezda@gmail.com>
2014-10-29 00:18:57 -0400

    Export as global to test in client app.

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
-var nodeCrypto = require('crypto') //Node.js or Browserify (browser)
-var Buffer = require('buffer').Buffer
-var BigInteger = require('bigi')
+var nodeCrypto = require('crypto');
+var Buffer = require('buffer').Buffer;
+var BigInteger = require('bigi');
 
-var ecdsa = require('ecdsa')
-var sr = require('secure-random') //npm install --save secure-random@1.x
-var CoinKey = require('coinkey') //npm install --save coinkey@0.1.0
-var bs58 = require('bs58')
+var ecdsa = require('ecdsa');
+var sr = require('secure-random');
+var CoinKey = require('coinkey');
 
-var privateKey = sr.randomBuffer(32)
-var ck = new CoinKey(privateKey, true) // true => compressed public key / addresses
-
-var msg = new Buffer("hello world!", 'utf8')
-var shaMsg = nodeCrypto.createHash('sha256').update(msg).digest()
-var signature = ecdsa.sign(shaMsg, ck.privateKey)
-var serializedSig = signature.r.toString() + signature.s.toString()
-
-console.log("Serialized sig: " + serializedSig)
-var rString = serializedSig.slice(0, 77)
-var sString = serializedSig.slice(77, 154)
-var reconstructedSig = {
-  r: new BigInteger(rString),
-  s: new BigInteger(sString)
+function generateKeys(){
+  var privateKey = sr.randomBuffer(32);
+  return new CoinKey(privateKey, true); // true => compressed public key / addresses
 }
-console.log("Verify: " + ecdsa.verify(shaMsg, reconstructedSig, ck.publicKey))
+function generateSha(buffer){
+  return nodeCrypto.createHash('sha256').update(buffer).digest();
+}
+function generateSignature(shaMsg, privateKey){
+  return ecdsa.sign(shaMsg, privateKey);
+}
+function serializeSignature(signature){
+  return signature.r.toString() + signature.s.toString();
+}
+function deserializeSignature(serializedSignature){
+  var rString = serializedSignature.slice(0, 77);
+  var sString = serializedSignature.slice(77, 154);
+  return {
+    r: new BigInteger(rString),
+    s: new BigInteger(sString)
+  }
+}
+function verifySignature(shaMsg, signature, publicKey){
+  return ecdsa.verify(shaMsg, reconstructedSig, ck.publicKey);
+}
+window.sigUtils = {
+  generateKeys: generateKeys,
+  generateSha: generateSha,
+  generateSignature: generateSignature,
+  serializeSignature: serializeSignature,
+  deserializeSignature: deserializeSignature,
+  verifySignature: verifySignature
+}
